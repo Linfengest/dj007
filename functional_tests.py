cf63b481b20fadf303f77728cf1dc56cd0a6c2a1
yuewengao <157793728@qq.com>
2019-08-25 17:36:44 +0800

    django:1.0.8 have myself change No change in 1.0.8

--- functional_tests.py
+++ functional_tests.py
@@ -10,10 +10,18 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    #检查重构
     def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(row_text,[row.text for row in rows])
+
+    #输入重构
+    def inputtext(self,write_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(write_text)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #听说一个很酷的在线待办事项应用
@@ -32,26 +40,33 @@ class NewVisitorTest(unittest.TestCase):
                         'Enter a to-do item'
                         )
 
+        #输入两个待办事项并检查
+        inputtext("Buy peacock feathers")
+        inputtext("Use peacock feather to make a fly")
+        check_for_row_in_list_table("Buy peacock feathers")
+        check_for_row_in_list_table("Use peacock feather to make a fly")
+
+        self.fail("Finish the test!")
         #在文本框中输入“Buy peacock feathers"
-        inputbox.send_keys('Buy peacock feathers')
+        #inputbox.send_keys('Buy peacock feathers')
 
         #按下回车，页面更新，
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        #inputbox.send_keys(Keys.ENTER)
+        #time.sleep(1)
 
         #页面还有输入框，继续输入
         #输入“Use peacock feather to make a fly"
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feather to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        #inputbox = self.browser.find_element_by_id('id_new_item')
+        #inputbox.send_keys('Use peacock feather to make a fly')
+        #inputbox.send_keys(Keys.ENTER)
+        #time.sleep(1)
 
         #表格显示‘Buy peacock feathers Use peacock feather to make a fly’
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue('Buy peacock feathers',[row.text for row in rows])
-        self.assertTrue('Use peacock feather to make a fly',[row.text for row in rows])
-        self.fail("Finish the test!")
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertTrue('Buy peacock feathers',[row.text for row in rows])
+        #self.assertTrue('Use peacock feather to make a fly',[row.text for row in rows])
+
 
 if __name__=='__main__':
      unittest.main(warnings='ignore')
