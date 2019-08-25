820710242a6fa82cf8e33be11a56822961cfca75
yuewengao <157793728@qq.com>
2019-08-25 15:02:23 +0800

    django：1.0.5 functional test input item

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import unittest
-
-
+import time
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -11,23 +11,57 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        #听说一个很酷的在线待办事项应用
+        #进去看看首页
         self.browser.get("http://localhost:8000")
 
-        self.assertIn('To-Do', self.browser.title)
+        #注意到首页标题和头部都有 'To-Do'
+        self.assertIn('To-Do',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        #应用邀请输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                        inputbox.get_attribute('placeholder'),
+                        'Enter a to-do item'
+                        )
+
+        #在文本框中输入“Buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+
+        #按下回车，页面更新，表格显示‘1：Buy peacock feathers’
+        inputbox.send_keys(keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                        any(row.text == '1：Buy peacock feathers')
+                        )
+
         self.fail("Finish the test!")
 
 if __name__=='__main__':
      unittest.main(warnings='ignore')
-#很酷的再现办事项应用
-#我要去看以下这个网站
-#browser.get("http://localhost:8000")
 
-#我注意到网页标题和头部都含有“To-Do”这个词
-#assert 'To-Do' in browser.title
 
-#应用邀请我输入一个待办事项
 
-#我在文本框输入“buy peacock feathers"
-#我的爱好是用蚊子作鱼饵钓鱼
-#browser = webdriver.Firefox()
-#browser.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
